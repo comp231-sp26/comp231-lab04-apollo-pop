2f88672eac80a447ab7ff69fd0cd8b43c7230321
Apollo Popowitz <apollop2005@hotmail.com>
2026-05-01 23:16:23 -0500

    lab 4 completed

--- part1/part1.s
+++ part1/part1.s
@@ -3,14 +3,71 @@
 
 _start:                             
           // initialization code here
-          	b		display
+        ldr r2, =0xFF200050    	// key register address
+   	   	ldr r3, =0xFF200020    	// hex0 address
+    	mov r5, #0             	// current displayed number
 
 loop:
+		ldr r4, [r2]           	// read keys
+    	and r0, r4, #1			// check key0
+    	cmp r0, #0
+    	bne key0
+    	and r0, r4, #2			// check key1
+    	cmp r0, #0
+   		bne key1
+   		and r0, r4, #4			// check key2
+   		cmp r0, #0
+   		bne key2
+    	and r0, r4, #8			// check key3
+    	cmp r0, #0
+   		bne key3
+        b loop
+          
+key0:
+    	mov r5, #0				// reset to 0
+     	mov r0, r5				
+    	bl seg7_code			// get bit pattern
+    	str r0, [r3]			// display
+wait0:
+    	ldr r0, [r2]
+    	and r0, r0, #1			// wait for release
+    	cmp r0, #0
+    	bne wait0
+    	b loop
 
-          // loop code here
+key1:
+    	add r5, r5, #1			// increment by 1
+    	mov r0, r5
+    	bl seg7_code
+    	str r0, [r3]
+wait1:
+    	ldr r0, [r2]			// decrease by 1
+    	and r0, r0, #2
+    	cmp r0, #0
+    	bne wait1
+    	b loop
 
-          b loop
+key2:
+    	sub r5, r5, #1			// blank display
+    	mov r0, r5
+    	bl seg7_code
+    	str r0, [r3]
+wait2:
+    	ldr r0, [r2]
+    	and r0, r0, #4
+    	cmp r0, #0
+    	bne wait2
+    	b loop
 
+key3:
+    	mov r0, #0 
+   	    str r0, [r3]
+wait3:
+    	ldr r0, [r2]
+    	and r0, r0, #8
+    	cmp r0, #0
+    	bne wait3
+    	b loop
 
 bit_codes:  .byte   0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110
             .byte   0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111
@@ -24,6 +81,6 @@ seg7_code:  ldr     r1, =bit_codes
 display:    ldr     r8, =0xff200020 // base address of hex3-hex0
 			mov		r0, #0			// set r0 to 0
             bl      seg7_code    	// returns r0 converted to a bit code in r0   
-            str		r8, r0   
+            str		r0, [r8]   
           
 .end
--- part3/part3.s
+++ part3/part3.s
@@ -1,14 +1,66 @@
 .text
 .global _start
 
-_start:                             
-          // initialization code here
+_start:
+    	ldr r2, =0xFF20005C        // edge capture register
+    	ldr r3, =0xFF200020        // HEX display address
+    	ldr r4, =0xFFFEC600        // timer base address
+    	mov r5, #0                 // count = 0
+    	mov r6, #0                 // running flag 
+    
+
+	    ldr r0, =50000000          // 0.25 seconds at 200MHz
+    	str r0, [r4]               // write to load register (offset 0)
+    	mov r0, #0b011             // e=1 a=1 i=0
+    	str r0, [r4, #8]           // write to control register
 
 loop:
+		mov r0, r5
+    	bl DIVIDE
+    	mov r8, r1
+    	bl seg7_code
+    	mov r9, r0
+    	mov r0, r8
+    	bl seg7_code
+    	lsl r0, r0, #8
+    	orr r0, r0, r9
+    	str r0, [r3]
+    
 
-          // loop code here
+	    ldr r0, [r2]
+    	cmp r0, #0
+    	beq check_timer
+    	eor r6, r6, #1             // toggle running
+    	str r0, [r2]               // clear edge
 
-          b loop
+check_timer:
+    	cmp r6, #0                 // if not running, skip
+    	beq loop
+    
+    	ldr r0, [r4, #0xC]         // read interrupt status
+    	and r0, r0, #1             // check f bit
+    	cmp r0, #0
+    	beq loop                   // timer not done yet, loop
+    
+    	mov r0, #1                 // clear f bit
+    	str r0, [r4, #0xC]
+    
+    	add r5, r5, #1             // increment count
+    	cmp r5, #100               // check if 99
+    	blt loop
+    	mov r5, #0
+    	b loop
+
+DIVIDE: MOV     R1, #0
+
+CONT:   CMP     R0, #10
+        BLT     DIV_END
+        SUB     R0, R0, #10
+        ADD     R1, R1, #1
+        B       CONT
+
+DIV_END:   
+        BX      LR
 
 
 bit_codes:  .byte   0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110
